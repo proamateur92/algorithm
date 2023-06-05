b8be2cc8afe74b3e37a51941e48b680a05715935
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2023-06-05 16:04:10 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        HashMap<String, Integer> m = new HashMap<>();
+        
+        for(int i = 0; i < name.length; i++) {
+            m.put(name[i], yearning[i]);    
+        }
+        
+        for(int i = 0; i < photo.length; i++) {
+            int score = 0;
+            
+            for(int j = 0; j < photo[i].length; j++) {
+                if(m.get(photo[i][j]) == null) continue;
+                
+                score += m.get(photo[i][j]);
+            }
+            
+            answer[i] = score;
+        }
+        
+        return answer;
+    }
+}
