f90f393370d7fb800dc7105f78a99f5fa711cec5
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2023-06-08 15:36:01 +0900

    [Silver IV] Title: 주몽, Time: 208 ms, Memory: 16864 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        
+        int[] A = new int[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for(int i=0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());    
+        }
+        
+        Arrays.sort(A);
+
+        int count = 0, i = 0, j = n -1;
+        
+        while(i < j) {
+            if(m == A[i] + A[j]) {
+                count++;
+                i++;
+                j--;
+            } else if(m < A[i] + A[j]) {
+                j--;
+            } else if(m > A[i] + A[j]) {
+                i++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
